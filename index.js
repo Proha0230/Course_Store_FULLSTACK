1a14bb08523fdbe0c64ca739a750d8d21d76f618
Ivan <Priest.ivan14@gmail.com>
2023-05-06 16:32:07 +0300

    Законченное приложение. Написано на чистом JS + NodeJS + Express.js + materializecss

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-// const path = require('path')
+const path = require('path')
 const app = express()
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
+const cardRoutes = require('./routes/card')
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -14,11 +15,12 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
 
 
 const PORT = process.env.PORT || 3000
